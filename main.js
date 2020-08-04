8ea4d5920d57574bd15e0f3dee96eb769a88ba4f
catruge <49883288+catruge@users.noreply.github.com>
2020-08-04 17:58:47 -0400

    Replace try/catch with if statement

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ const buttons = {
 };
 
 document.onkeypress = (keypress) => {
-  try {
+  if (keypress.key in buttons) {
     const opacity = buttons[keypress.key].style.opacity;
     buttons[keypress.key].style.opacity = opacity === '1' || opacity === '' ? '0.1' : '1';
-  } catch {}
+  }
 }
